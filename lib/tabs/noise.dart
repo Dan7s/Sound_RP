590e2277ff1b04801bc5bacb81e84f4b8642ffa9
dan7s <daniel.skadlubowicz@gmail.com>
2020-06-29 13:03:14 +0200

    Adding first features to "Instant Noise" tab.

--- lib/tabs/noise.dart
+++ lib/tabs/noise.dart
@@ -1,23 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
-class NoiseTab extends StatelessWidget {
+class NoiseTab extends StatefulWidget {
 	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			backgroundColor: Colors.black,
-			body: Container(
-				child: Center(
-					child: Column(
-						mainAxisAlignment: MainAxisAlignment.center,
-						children: <Widget>[
-							Text(
-								'Instant Noise List',
-								style: TextStyle(color: Colors.green),
-							),
-						],
-					),
-				),
-			),
+	NoiseState createState() => NoiseState();
+}
+
+List<Noise> noises = [];
+int idCounterN = 0;
+
+class NoiseState extends State<NoiseTab> with AutomaticKeepAliveClientMixin<NoiseTab> {
+	// main noise tab obj
+	@override
+	bool get wantKeepAlive => true;
+
+	void initState() {
+		_initDownloader();
+		super.initState();
+	}
+
+	void _initDownloader() async {
+		WidgetsFlutterBinding.ensureInitialized();
+		await FlutterDownloader.initialize(
+				debug: true // optional: set false to disable printing logs to console
 		);
 	}
+
+	Widget build(BuildContext context){
+		return Scaffold(
+				backgroundColor: Colors.black,
+				body: GridView.builder(
+						itemCount: noises.length,
+						gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( crossAxisCount: 3,),
+						itemBuilder: (context, index) {
+								return Card(
+									color: Colors.grey[800],
+									child: Center( child: Text( noises[index].title, style: TextStyle(color: Colors.white)),),
+								);
+						},
+				),
+				floatingActionButton: FloatingActionButton(			//adding action button
+					onPressed: () async {
+						addNoise();
+						setState(() {});
+					},
+					child: Icon(Icons.more_vert),
+					tooltip: 'Add noise',
+					backgroundColor: Colors.green,
+				),
+		);
+	}
+}
+
+class Noise {
+	String title;
+	String path;
+	int id;
+
+	Noise(this.title, this.path, this.id);
+}
+
+void addNoise() {
+	noises.add(new Noise("Test " + idCounterN.toString(), "test", idCounterN));
+	idCounterN += 1;
 }
